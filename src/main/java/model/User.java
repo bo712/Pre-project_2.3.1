dd066e251d37b9cd1bc145b9659c6dd1c5bf4196
Konstantin <linuksoid@gmail.com>
2020-05-02 00:48:12 +0300

    Create User.java

---
+++ src/main/java/model/User.java
@@ -0,0 +1,64 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "lastName")
+    String lastName;
+
+    @Column(name = "salary")
+    int salary;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, int salary, long id) {
+        this.name = name;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
