a81422527d9d9df09cddb7bf95056a953116bfab
Konstantin <linuksoid@gmail.com>
2020-05-05 22:36:30 +0300

    remove @ComponentScan

--- src/main/java/web/config/HibernateConfig.java
+++ src/main/java/web/config/HibernateConfig.java
@@ -2,7 +2,6 @@ package web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -19,7 +18,6 @@ import java.util.Properties;
 @Configuration
 @PropertySource({"classpath:db.properties", "classpath:hibernate.properties"})
 @EnableTransactionManagement
-@ComponentScan("web")
 public class HibernateConfig {
 
     @Autowired
