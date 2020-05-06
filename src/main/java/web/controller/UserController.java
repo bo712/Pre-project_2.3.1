60dd0964b159a17d4d4b6c3326cede0aef9cef4c
Konstantin <linuksoid@gmail.com>
2020-05-06 03:11:50 +0300

    added dependencies, classes and methods for SpringSecurity

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 import web.model.User;
 import web.service.IUserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -24,6 +26,21 @@ public class UserController {
     @RequestMapping("/")
     public String getIndex() {
         return "index";
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC-SECURITY application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "hello";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String loginPage() {
+        return "login";
     }
 
     @RequestMapping("/users")
---
+++ src/webapp/WEB-INF/pages/login.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<form method="post" action="<c:url value="/login"/>">
+    <input name="j_username"/>
+    <input name="j_password"/>
+    <input type="submit"/>
+</form>
+
+</body>
+</html>
