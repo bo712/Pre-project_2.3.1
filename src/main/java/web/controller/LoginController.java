6cff803f9d39e29414617314dd38c5965d00217b
Konstantin <linuksoid@gmail.com>
2020-05-12 13:45:05 +0300

    duplicate method call removed

--- src/main/java/web/controller/LoginController.java
+++ src/main/java/web/controller/LoginController.java
@@ -40,7 +40,6 @@ public class LoginController {
 
     @GetMapping(value = "register")
     public String registerPage() {
-        roleService.setDefaultRoles();
         return "register";
     }
 
